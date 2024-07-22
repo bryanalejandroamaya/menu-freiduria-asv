12b3120ce293eb5bf52b48d39979878538a8af03
Bryan Amaya <bryan.amaya4242@gmail.com>
2024-07-22 22:29:35 +0200

    provisional menu completed :+1:

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import bigScreenBackground from './../public/BigScreenBackgroundv2.png';
-import smallScreenBackground from './../public/SmallScreenBackground.webp';
-import './App.css'
+import smallScreenBackground from './../public/SmallScreenBackground.jpeg';
+import smallScreenMenu from './../public/SmallScreenMenu.png';
+import './App.css';
 
 function App() {
   const [currentLogo, setCurrentLogo] = useState<string>(bigScreenBackground); // Estado para la imagen actual
+  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(window.innerWidth < 1280); // Estado para determinar si es pantalla pequeña
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 600) {
+      if (window.innerWidth < 1280) {
         setCurrentLogo(smallScreenBackground); // Cambia a la imagen para pantallas pequeñas
+        setIsSmallScreen(true); // Marca como pantalla pequeña
       } else {
         setCurrentLogo(bigScreenBackground); // Vuelve a la imagen original
+        setIsSmallScreen(false); // Marca como pantalla grande
       }
     };
 
@@ -27,11 +31,16 @@ function App() {
 
   return (
     <>
-      <div className="app"> 
-        <img src={currentLogo} className="background"></img>
+      <div className="app">
+        <img src={currentLogo} className={`background ${isSmallScreen ? 'background-fixed' : ''}`} alt="background" />
+        {isSmallScreen && (
+          <div className="menu">
+            <img src={smallScreenMenu} alt="small screen menu" />
+          </div>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
